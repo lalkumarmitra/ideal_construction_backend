5014dfd12e4e2a3504afb8631b7d290a78a5d835
Lal Kumar mitra <apple@MacBook-Air.local>
2025-03-22 15:43:26 +0530

    casual

--- system/app/Http/Controllers/UserController.php
+++ system/app/Http/Controllers/UserController.php
@@ -90,7 +90,7 @@ class UserController extends Controller
             return [
                 'message' => 'Users Fetched Successfully',
                 'data' => [
-                    'request'=>$request->search_query,
+                    'request'=>$request->all(),
                     'users' => $users->items(),
                     'current_page' => $users->currentPage(),
                     'per_page' => $users->perPage(),
